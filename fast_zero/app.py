ad3523806c59089d9510faa811daf4154ef23881
Rodrigo Paterlini <rodpater@gmail.com>
2025-05-04 20:09:48 -0300

    criado app post com fakeDB

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -4,16 +4,32 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserInDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     """
     This function reads the root directory of the FastZero application.
-    It is a placeholder for future functionality.
     """
 
     return {'message': 'Welcome to FastZero!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    """
+    This function creates a new user.
+    """
+    user_with_id = UserInDB(
+        id=len(database) + 1,
+        **user.model_dump(),
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,27 @@ from http import HTTPStatus
 from fastapi.testclient import TestClient
 from fast_zero.app import app
 
-client = TestClient(app)
-
 
 def test_read_root():
-    response = client.get("/") # This is the root endpoint - Arrange
-    assert response.status_code == HTTPStatus.OK # - Act
+    client = TestClient(app)  #This is the root endpoint - Arrange
+    response = client.get("/")  # - Act
+    assert response.status_code == HTTPStatus.OK # - Assert
     assert response.json() == {"message": "Welcome to FastZero!"} # - Assert
+
+
+def test_create_user():
+    client = TestClient(app)  # This is the create user endpoint - Arrange
+    response =  client.post( # test for UserSchema endpoitnt
+        "/users/",
+        json={
+            "username": 'testuser',
+            "email": 'user@email.com',
+            "password": 'securepassword',
+        }
+    )  # - Act
+    assert response.status_code == HTTPStatus.CREATED  # - Assert status code
+    assert response.json() == { # - Assert validar UserPublic
+        "id": 1,
+        "username": 'testuser',
+        "email": 'user@email.com',
+    }  # - Assert response data
