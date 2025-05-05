1229463232803b45a43d03cf9861566aff088612
Rodrigo Paterlini <rodpater@gmail.com>
2025-05-04 23:16:20 -0300

    criado fixture para chamada cliente

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,24 @@
 from http import HTTPStatus
+import pytest
 from fastapi.testclient import TestClient
 from fast_zero.app import app
 
+@pytest.fixture() # - Fixture to create a test client  Arrange
+def client():
+    """
+    This fixture creates a test client for the FastAPI application.
+    """
+    return TestClient(app)
+# Compare this snippet from fast_zero/schemas.py:
 
-def test_read_root():
-    client = TestClient(app)  #This is the root endpoint - Arrange
+p
+def test_read_root(client):
     response = client.get("/")  # - Act
     assert response.status_code == HTTPStatus.OK # - Assert
     assert response.json() == {"message": "Welcome to FastZero!"} # - Assert
 
 
-def test_create_user():
-    client = TestClient(app)  # This is the create user endpoint - Arrange
+def test_create_user(client):
     response =  client.post( # test for UserSchema endpoitnt
         "/users/",
         json={
