4583d499a8feaf622543effb725889369e5659cf
Rodrigo Paterlini <rodpater@gmail.com>
2025-05-04 23:41:07 -0300

    criado arquivo conftest para armazenar fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 from fast_zero.app import app
 
-@pytest.fixture()  # - Fixture to create a test client  Arrange
+@pytest.fixture() # - Fixture to create a test client  Arrange
 def client():
     """
     This fixture creates a test client for the FastAPI application.
     """
     return TestClient(app)
 # Compare this snippet from fast_zero/schemas.py:
-
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,4 @@
 from http import HTTPStatus
-import pytest
-from fastapi.testclient import TestClient
-from fast_zero.app import app
-
-@pytest.fixture() # - Fixture to create a test client  Arrange
-def client():
-    """
-    This fixture creates a test client for the FastAPI application.
-    """
-    return TestClient(app)
-# Compare this snippet from fast_zero/schemas.py:
 
 
 def test_read_root(client):
